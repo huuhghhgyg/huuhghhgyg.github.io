dbfcc1509affdf13bf75cf7cca87a9a0c433b7e1
huuhghhgyg <1657936673@qq.com>
2021-07-11 21:39:06 +0800

    Site updated: 2021-07-11 21:39:05

--- manifest.js
+++ manifest.js
@@ -9,8 +9,10 @@
 // Code to handle install prompt on desktop
 
 let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+if (document.getElementById('add-button')){
+const addBtn = document.querySelector('#add-button');
+    addBtn.style.display = 'none';
+}
 
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
